3fb340e05f70766507cce8e40e12e49e7b748a60
Emmanuel Blot <emmanuel.blot@sifive.com>
2020-08-11 15:16:07 +0200

    Make CMake a bit easier to use w/ sub-directories

--- tests/hello/src/hello.c
+++ tests/hello/src/hello.c
@@ -1,12 +1,30 @@
+#include <stdint.h>
+#include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
 #include "metal/machine.h"
 #include "metal/tty.h"
+#include "api/hardware/v0.5/random/hca_trng.h"
+#include "api/hardware/v0.5/sifive_hca-0.5.x.h"
+#include "api/hardware/hca_utils.h"
+#include "api/hardware/hca_macro.h"
 
 #define UART0 __METAL_DT_SERIAL_20000000_HANDLE
 #define UART1 __METAL_DT_SERIAL_20008000_HANDLE
 
-int main() {
+#define DEBUG_HCA
+
+#ifdef DEBUG_HCA
+# define LPRINTF(_f_, _l_, _msg_, ...) \
+    printf("%s[%d] " _msg_ "\n", _f_, _l_, ##__VA_ARGS__)
+# define PRINTF(_msg_, ...) \
+    LPRINTF(__func__, __LINE__, _msg_, ##__VA_ARGS__)
+#else // DEBUG_HCA
+# define LPRINTF(_f_, _l_, _msg_, ...)
+# define PRINTF(_msg_, ...)
+#endif
+
+int main(void) {
 
     printf("Hello, World!\n");
 
