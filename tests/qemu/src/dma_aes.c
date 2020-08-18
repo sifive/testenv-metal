bd7b973506383287228e5b3cfbaf4ca4c288a6cf
Emmanuel Blot <emmanuel.blot@sifive.com>
2020-08-18 10:55:52 +0200

    Test DMA AES ECB IRQ mode

--- tests/qemu/src/dma_aes.c
+++ tests/qemu/src/dma_aes.c
@@ -330,7 +330,6 @@ _test_aes_dma_poll(const uint8_t * ref_d, const uint8_t * ref_s, uint8_t * dst,
     size_t chunk = length/repeat;
 
     // DMA config
-    PRINTF("src: %p, dst: %p, len: %zu", src, dst, length);
     METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_DMA_SRC) = (uintptr_t)src;
     METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_DMA_DEST) = (uintptr_t)dst;
     METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_DMA_LEN) = length/DMA_BLOCK_SIZE;
@@ -397,7 +396,6 @@ _test_aes_dma_poll(const uint8_t * ref_d, const uint8_t * ref_s, uint8_t * dst,
     }
 
     // DMA config
-    PRINTF("src: %p, dst: %p, len: %zu", src, dst, length);
     METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_DMA_SRC) = (uintptr_t)dst;
     METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_DMA_DEST) = (uintptr_t)src;
     METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_DMA_LEN) = length/DMA_BLOCK_SIZE;
@@ -422,6 +420,293 @@ _test_aes_dma_poll(const uint8_t * ref_d, const uint8_t * ref_s, uint8_t * dst,
         // the guest code to be re-scheduled before the VM DMA completion
         TEST_ASSERT_GREATER_THAN_size_t_MESSAGE(
             1000u, dma_loop, "VM may have freeze guest code execution");
+    }
+
+    if ( ref_s ) {
+        const uint8_t * ptr = src;
+        for (unsigned int ix=0; ix<repeat; ix++) {
+            if ( memcmp(ptr, ref_s, chunk) ) {
+                DUMP_HEX("Invalid AES:", ptr, chunk);
+                DUMP_HEX("Ref:        ", ref_s, chunk);
+                TEST_FAIL_MESSAGE("AES decryption mismatch");
+            }
+            ptr += chunk;
+        }
+    }
+}
+
+static void
+_hca_irq_handler(int id, void * opaque)
+{
+    struct worker * work = (struct worker *)opaque;
+
+    uint32_t cr = METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_CR);
+
+    if ( cr & (HCA_REGISTER_CR_DMADIS_MASK <<
+               HCA_REGISTER_CR_DMADIS_OFFSET) ) {
+        work->wk_dma_count += 1u;
+        work->wk_dma_total += 1u;
+        //puts("^D\n");
+    }
+
+    if ( cr & (HCA_REGISTER_CR_CRYPTODIS_MASK <<
+               HCA_REGISTER_CR_CRYPTODIS_OFFSET) ) {
+        work->wk_crypto_count += 1u;
+        work->wk_crypto_total += 1u;
+        //puts("^S\n");
+    }
+}
+
+static void
+_timer_irq_handler(int id, void * opaque) {
+    struct metal_cpu *cpu = (struct metal_cpu *)opaque;
+    metal_cpu_set_mtimecmp(cpu, metal_cpu_get_mtime(cpu)+HEART_BEAT_TIME);
+    // puts("^T\n");
+}
+
+static void
+_hca_irq_init(struct worker * work)
+{
+    // Lets get the CPU and and its interrupt
+    struct metal_cpu *cpu;
+    cpu = metal_cpu_get(metal_cpu_get_current_hartid());
+    TEST_ASSERT_NOT_NULL_MESSAGE(cpu, "Cannot get CPU");
+
+    struct metal_interrupt *cpu_intr;
+    cpu_intr = metal_cpu_interrupt_controller(cpu);
+    TEST_ASSERT_NOT_NULL_MESSAGE(cpu_intr, "Cannot get CPU controller");
+    metal_interrupt_init(cpu_intr);
+
+    struct metal_interrupt * plic;
+    plic = metal_interrupt_get_controller(METAL_PLIC_CONTROLLER, 0);
+    TEST_ASSERT_NOT_NULL_MESSAGE(plic, "Cannot get PLIC");
+     metal_interrupt_init(plic);
+
+    int rc;
+    rc = metal_interrupt_register_handler(plic, HCA_ASD_IRQ_CHANNEL,
+                                          &_hca_irq_handler, work);
+    TEST_ASSERT_FALSE_MESSAGE(rc, "Cannot register IRQ handler");
+
+    rc = metal_interrupt_enable(plic, HCA_ASD_IRQ_CHANNEL);
+    TEST_ASSERT_FALSE_MESSAGE(rc, "Cannot enable IRQ");
+
+    metal_interrupt_set_threshold(plic, 1);
+    metal_interrupt_set_priority(plic, HCA_ASD_IRQ_CHANNEL, 2);
+
+    // use a timer IRQ as an easier workaround for a time vulnarability
+    // issue between WFI instruction and ISR. To avoid being stuck in WFI,
+    // add an hearbeat.
+    struct metal_interrupt *tmr_intr;
+    tmr_intr = metal_cpu_timer_interrupt_controller(cpu);
+    if ( !tmr_intr ) {
+        return;
+    }
+    metal_interrupt_init(tmr_intr);
+
+    int tmr_id;
+    tmr_id = metal_cpu_timer_get_interrupt_id(cpu);
+    rc = metal_interrupt_register_handler(tmr_intr, tmr_id, _timer_irq_handler,
+                                          cpu);
+    TEST_ASSERT_FALSE_MESSAGE(rc, "Cannot register IRQ handler");
+
+    metal_cpu_set_mtimecmp(cpu, metal_cpu_get_mtime(cpu)+HEART_BEAT_TIME);
+    metal_interrupt_enable(tmr_intr, tmr_id);
+    metal_interrupt_enable(cpu_intr, 0);
+}
+
+static void
+_hca_irq_fini(void)
+{
+    int rc;
+
+    struct metal_interrupt * plic;
+    plic = metal_interrupt_get_controller(METAL_PLIC_CONTROLLER, 0);
+    TEST_ASSERT_NOT_NULL_MESSAGE(plic, "Cannot get PLIC");
+
+    rc = metal_interrupt_disable(plic, HCA_ASD_IRQ_CHANNEL);
+    TEST_ASSERT_FALSE_MESSAGE(rc, "Cannot diable IRQ");
+
+
+    // clear interrupts
+    _hca_updreg32(METAL_SIFIVE_HCA_CR, 0,
+                  HCA_REGISTER_CR_CRYPTODIE_OFFSET,
+                  HCA_REGISTER_CR_CRYPTODIE_MASK);
+    _hca_updreg32(METAL_SIFIVE_HCA_CR, 0,
+                  HCA_REGISTER_CR_OFIFOIE_OFFSET,
+                  HCA_REGISTER_CR_OFIFOIE_MASK);
+    _hca_updreg32(METAL_SIFIVE_HCA_CR, 0,
+                  HCA_REGISTER_CR_DMADIE_OFFSET,
+                  HCA_REGISTER_CR_DMADIE_MASK);
+}
+
+static void
+_test_aes_dma_irq(const uint8_t * ref_d, const uint8_t * ref_s, uint8_t * dst,
+                   uint8_t * src, size_t length, size_t repeat,
+                   struct worker * work) {
+    uint32_t reg;
+
+    TEST_ASSERT_EQUAL_MESSAGE(((uintptr_t)src) & ((DMA_ALIGNMENT) - 1u), 0,
+                              "Source is not aligned on a DMA boundary");
+    TEST_ASSERT_EQUAL_MESSAGE(((uintptr_t)dst) & ((DMA_ALIGNMENT) - 1u), 0,
+                              "Destination is not aligned on a DMA boundary");
+    TEST_ASSERT_EQUAL_MESSAGE(length & (DMA_BLOCK_SIZE - 1u), 0,
+                              "Length is not aligned on a DMA block size");
+
+    reg = METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_HCA_REV);
+    if ( ! reg ) {
+        PRINTF("HCA rev: %08x", reg);
+        TEST_FAIL_MESSAGE("HCA rev is nil");
+    }
+
+    reg = METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_AES_REV);
+    if ( ! reg ) {
+        PRINTF("SHA rev: %08x", reg);
+        TEST_FAIL_MESSAGE("AES rev is nil");
+    }
+
+    // FIFO mode: AES
+    _hca_updreg32(METAL_SIFIVE_HCA_CR, 0,
+                  HCA_REGISTER_CR_IFIFOTGT_OFFSET,
+                  HCA_REGISTER_CR_IFIFOTGT_MASK);
+
+    // FIFO endianess: natural order
+    _hca_updreg32(METAL_SIFIVE_HCA_CR, 1,
+                  HCA_REGISTER_CR_ENDIANNESS_OFFSET,
+                  HCA_REGISTER_CR_ENDIANNESS_MASK);
+
+    // IRQ: not on Crypto done
+    _hca_updreg32(METAL_SIFIVE_HCA_CR, 0,
+                  HCA_REGISTER_CR_CRYPTODIE_OFFSET,
+                  HCA_REGISTER_CR_CRYPTODIE_MASK);
+    // IRQ: not on output FIFO not empty
+    _hca_updreg32(METAL_SIFIVE_HCA_CR, 0,
+                  HCA_REGISTER_CR_OFIFOIE_OFFSET,
+                  HCA_REGISTER_CR_OFIFOIE_MASK);
+    // IRQ: not on DMA done
+    _hca_updreg32(METAL_SIFIVE_HCA_CR, 1,
+                  HCA_REGISTER_CR_DMADIE_OFFSET,
+                  HCA_REGISTER_CR_DMADIE_MASK);
+
+    // sanity check
+    reg = METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_CR);
+    TEST_ASSERT_EQUAL_MESSAGE(reg &
+                              (HCA_CR_IFIFO_EMPTY_BIT|HCA_CR_OFIFO_EMPTY_BIT),
+                              HCA_CR_IFIFO_EMPTY_BIT|HCA_CR_OFIFO_EMPTY_BIT,
+                              "FIFOs are not empty");
+    TEST_ASSERT_EQUAL_MESSAGE(reg &
+                              (HCA_CR_IFIFO_FULL_BIT|HCA_CR_OFIFO_FULL_BIT),
+                              0u,
+                              "FIFOs are full");
+
+    // AES mode: ECB
+    _hca_updreg32(METAL_SIFIVE_HCA_AES_CR, 0u,
+                  HCA_REGISTER_AES_CR_MODE_OFFSET,
+                  HCA_REGISTER_AES_CR_MODE_MASK);
+    // AES key size: 128 bits
+    _hca_updreg32(METAL_SIFIVE_HCA_AES_CR, 0u,
+                  HCA_REGISTER_AES_CR_KEYSZ_OFFSET,
+                  HCA_REGISTER_AES_CR_KEYSZ_MASK);
+    // AES process: encryption
+    _hca_updreg32(METAL_SIFIVE_HCA_AES_CR, 0u,
+                  HCA_REGISTER_AES_CR_PROCESS_OFFSET,
+                  HCA_REGISTER_AES_CR_PROCESS_MASK);
+    // AES init: no need
+    _hca_updreg32(METAL_SIFIVE_HCA_AES_CR, 0u,
+                  HCA_REGISTER_AES_CR_INIT_OFFSET,
+                  HCA_REGISTER_AES_CR_INIT_MASK);
+
+    // AES key
+    _hca_set_aes_key128(_KEY_AES128);
+
+    if ( _hca_aes_is_busy() ) {
+        TEST_FAIL_MESSAGE("AES HW is busy");
+    }
+
+    if ( _hca_dma_is_busy() ) {
+        TEST_FAIL_MESSAGE("DMA HW is busy");
+    }
+
+    memset(work, 0, sizeof(*work));
+
+    size_t chunk = length/repeat;
+
+    // DMA config
+    METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_DMA_SRC) = (uintptr_t)src;
+    METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_DMA_DEST) = (uintptr_t)dst;
+    METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_DMA_LEN) = length/DMA_BLOCK_SIZE;
+
+    // DMA start
+    _hca_updreg32(METAL_SIFIVE_HCA_DMA_CR, 1,
+                  HCA_REGISTER_DMA_CR_START_OFFSET,
+                  HCA_REGISTER_DMA_CR_START_MASK);
+
+    while( ! work->wk_dma_count ) {
+        __asm__ volatile ("wfi");
+    }
+    _hca_dma_clear_irq();
+
+    while ( _hca_aes_is_busy() ) {
+        // busy loop
+    }
+
+    if ( ref_d ) {
+        const uint8_t * ptr = dst;
+        for (unsigned int ix=0; ix<repeat; ix++) {
+            if ( memcmp(ptr, ref_d, chunk) ) {
+                DUMP_HEX("Invalid AES:", ptr, chunk);
+                DUMP_HEX("Ref:        ", ref_d, chunk);
+                TEST_FAIL_MESSAGE("AES encryption mismatch");
+            }
+            ptr += chunk;
+        }
+    }
+
+    // sanity check
+    reg = METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_CR);
+    TEST_ASSERT_EQUAL_MESSAGE(reg &
+                              (HCA_CR_IFIFO_EMPTY_BIT|HCA_CR_OFIFO_EMPTY_BIT),
+                              HCA_CR_IFIFO_EMPTY_BIT|HCA_CR_OFIFO_EMPTY_BIT,
+                              "FIFOs are not empty");
+    TEST_ASSERT_EQUAL_MESSAGE(reg &
+                              (HCA_CR_IFIFO_FULL_BIT|HCA_CR_OFIFO_FULL_BIT),
+                              0u,
+                              "FIFOs are full");
+
+    // AES process: decryption
+    _hca_updreg32(METAL_SIFIVE_HCA_AES_CR, 1u,
+                  HCA_REGISTER_AES_CR_PROCESS_OFFSET,
+                  HCA_REGISTER_AES_CR_PROCESS_MASK);
+
+    // AES key
+    _hca_set_aes_key128(_KEY_AES128);
+
+    if ( _hca_aes_is_busy() ) {
+        TEST_FAIL_MESSAGE("AES HW is busy");
+    }
+
+    if ( _hca_dma_is_busy() ) {
+        TEST_FAIL_MESSAGE("DMA HW is busy");
+    }
+
+    memset(work, 0, sizeof(*work));
+
+    // DMA config
+    METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_DMA_SRC) = (uintptr_t)dst;
+    METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_DMA_DEST) = (uintptr_t)src;
+    METAL_REG32(HCA_BASE, METAL_SIFIVE_HCA_DMA_LEN) = length/DMA_BLOCK_SIZE;
+
+    // DMA start
+    _hca_updreg32(METAL_SIFIVE_HCA_DMA_CR, 1,
+                  HCA_REGISTER_DMA_CR_START_OFFSET,
+                  HCA_REGISTER_DMA_CR_START_MASK);
+
+    while( ! work->wk_dma_count ) {
+        __asm__ volatile ("wfi");
+    }
+    _hca_dma_clear_irq();
+
+    while ( _hca_aes_is_busy() ) {
+        // busy loop
     }
 
     if ( ref_s ) {
@@ -468,10 +753,37 @@ TEST(dma_aes, aes_ecb_long_poll)
                        sizeof(_long_buf), repeat);
 }
 
+TEST(dma_aes, aes_ecb_irq)
+{
+    memcpy(_long_buf, _PLAINTEXT_ECB, sizeof(_PLAINTEXT_ECB));
+    _hca_irq_init(&_work);
+    _test_aes_dma_irq(_CIPHERTEXT_ECB, _PLAINTEXT_ECB, _dst_buf,
+                       _long_buf, sizeof(_PLAINTEXT_ECB), 1u, &_work);
+    _hca_irq_fini();
+}
+
+TEST(dma_aes, aes_ecb_long_irq)
+{
+    // test a long buffer, which is a repeated version of the short one.
+    // also take the opportunity to test src == dst buffers
+    size_t repeat = sizeof(_long_buf)/sizeof(_PLAINTEXT_ECB);
+    uint8_t * ptr = _long_buf;
+    for (unsigned int ix=0; ix<repeat; ix++) {
+        memcpy(ptr, _PLAINTEXT_ECB, sizeof(_PLAINTEXT_ECB));
+        ptr += sizeof(_PLAINTEXT_ECB);
+    }
+    _hca_irq_init(&_work);
+    _test_aes_dma_irq(_CIPHERTEXT_ECB, _PLAINTEXT_ECB, _long_buf, _long_buf,
+                       sizeof(_long_buf), repeat, &_work);
+    _hca_irq_fini();
+}
+
 TEST_GROUP_RUNNER(dma_aes)
 {
     RUN_TEST_CASE(dma_aes, aes_ecb_poll);
     RUN_TEST_CASE(dma_aes, aes_ecb_long_poll);
+    RUN_TEST_CASE(dma_aes, aes_ecb_irq);
+    RUN_TEST_CASE(dma_aes, aes_ecb_long_irq);
 }
 
 
