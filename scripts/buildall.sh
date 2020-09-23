5457e53262486530d6502aa1e61314232186f84d
Emmanuel Blot <emmanuel.blot@sifive.com>
2020-09-23 17:17:01 +0200

    Create a new helper script to work around GH action limitations

---
+++ scripts/buildall.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+#------------------------------------------------------------------------------
+# Build all BSPs/builds combinations.
+#
+# This script only exists to circumvent current GitHub actions limitations,
+# where the strategy matrix feature does not allow to perform global actions
+# before running the matrix combinations
+#------------------------------------------------------------------------------
+
+SCRIPT_DIR=$(dirname $0)
+DTS=""
+BUILDS="debug release"
+
+usage() {
+    NAME=`basename $0`
+    cat <<EOT
+$NAME [-h] [-a] [-s] [dts] ...
+
+ dts: the name of a dts file (w/o path or extension)
+
+ -h:  print this help
+ -a:  abort on first failed build (default: resume)
+ -s:  run static analyzer in addition to regular builds
+EOT
+}
+
+SA=0
+ABORT=0
+for arg in $*; do
+    case ${arg} in
+        -s)
+            SA=1
+            ;;
+        -a)
+            ABORT=1
+            ;;
+        -h)
+            usage
+            exit 0
+            ;;
+        -*)
+            ;;
+        *)
+            DTS="${DTS} ${arg}"
+            ;;
+    esac
+done
+if [ $SA -gt 0 ]; then
+    BUILDS="${BUILDS} static_analysis"
+fi
+
+test -n "${DTS}" || usage
+
+FAILURE=0
+for dts in ${DTS}; do
+    for build in ${BUILDS}; do
+        echo "[Building ${dts} in ${build}]"
+        ${SCRIPT_DIR}/build.sh ${dts} ${build}
+        if [ $? -ne 0 ]; then
+            echo "Build failed (${dts} in ${build})" >&2
+            if [ ${ABORT} -gt 0 ]; then
+                exit $?
+            else
+                FAILURE=1
+            fi
+        fi
+    done
+done
+
+if [ ${FAILURE} -ne 0 ]; then
+    echo "At least one build failed" >&2
+    exit 1
+fi
