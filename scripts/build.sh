4829e7631cd4e1d37de9f038a4d31444ff22ae39
Emmanuel Blot <emmanuel.blot@sifive.com>
2020-09-28 12:28:29 +0200

    Save a copy of error and warning message into an optional report log file

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,23 +15,30 @@ SCRIPT_DIR=$(dirname $0)
 usage () {
     NAME=`basename $0`
     cat <<EOT
-$NAME [-h] [-v] [-g] [debug|release|static_analysis] <bsp>
+$NAME [-h] [-v] [-g] [-r report] [debug|release|static_analysis] <bsp>
 
  bsp: the name of a BSP (see bsp/ directory)
 
  -h:  print this help
  -g:  github mode (filter compiler output)
+ -r:  copy all warnings and errors messages into a log file
  -v:  verbose (report all toolchain commands)
 EOT
 }
 
 filter_issues () {
-   egrep -v '^\[' | \
+    egrep -v '^\[' | \
         while read -r line; do
             if (echo "${line}" | grep -q 'warning:'); then
                 warning -n "WARNING: "
+                if [ -n "${REPORTLOG}" ]; then
+                    echo ${line} >> "${REPORTLOG}";
+                fi
             elif (echo "${line}" | grep -q 'error:'); then
                 error -n "ERROR: "
+                if [ -n "${REPORTLOG}" ]; then
+                    echo ${line} >> "${REPORTLOG}";
+                fi
             fi
             echo "${line}"
         done
@@ -43,20 +50,25 @@ SUBDIR=""
 BUILD="DEBUG"
 SA_DIR=""
 GHA=0
+REPORTLOG=""
 XBSP=""
-for arg in $*; do
-    case $arg in
+while [ $# -gt 0 ]; do
+    case "$1" in
         -h)
             usage
             ;;
         -g)
             GHA=1
             ;;
+        -r)
+            shift
+            REPORTLOG="$1"
+            ;;
         -v|VERBOSE)
             NINJA_OPTS="${NINJA_OPTS} -v"
             ;;
         *DEBUG|*RELEASE|*debug|*release)
-            BUILD=$(echo "${arg}" | tr [:upper:] [:lower:])
+            BUILD=$(echo "$1" | tr [:lower:] [:upper:])
             ;;
         STATIC_ANALYSIS|static_analysis)
             CMAKE_OPTS="${CMAKE_OPTS} -DSTATIC_ANALYSIS=1"
@@ -66,9 +78,10 @@ for arg in $*; do
             ;;
         *)
             if [ -z "${XBSP}" ]; then
-                XBSP="$arg"
+                XBSP="$1"
             fi
     esac
+    shift
 done
 test -n "${XBSP}" || die "XBSP should be specified"
 
