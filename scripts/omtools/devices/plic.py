5237a1c8e2004db6b830a38bd4c82c0c75c5cd88
Emmanuel Blot <emmanuel.blot@sifive.com>
2020-10-16 09:53:58 +0200

    Add Single register, multiple field support.

--- scripts/omtools/devices/plic.py
+++ scripts/omtools/devices/plic.py
@@ -1,7 +1,7 @@
 from copy import deepcopy
 from os.path import commonprefix
 from pprint import pprint
-from typing import Dict, List, Tuple
+from typing import Dict, List, Optional, Tuple
 from ..generator import OMSi5SisHeaderGenerator
 from ..model import OMDeviceMap, OMRegField
 
@@ -16,41 +16,98 @@ class OMSi5SisPlicHeaderGenerator(OMSi5SisHeaderGenerator):
                  debug: bool = False):
         super().__init__(regwidth, test, debug)
 
+    @classmethod
+    def compare_regfield(cls, reg_a: OMRegField, reg_b: OMRegField,
+                         stride: Optional[int] = None) -> bool:
+        if reg_a.size != reg_b.size:
+            print('size mismatch')
+            return False
+        if reg_a.reset != reg_b.reset:
+            print('reset mismatch')
+            return False
+        if reg_a.access != reg_b.access:
+            print('access mismatch')
+            return False
+        if stride is not None:
+            if reg_a.offset + stride != reg_b.offset:
+                print(f'stride mismatch {reg_a.offset} != '
+                      f'0x{reg_b.offset + stride:x}')
+                return False
+        return True
+
+    @classmethod
+    def compare_regfield_group(cls,
+            regs_a: List[OMRegField], regs_b: List[OMRegField],
+            stride: Optional[int] = None) -> bool:
+        if len(regs_a) != len(regs_b):
+            return False
+        offset = 0
+        for reg_a, reg_b in zip(regs_a, regs_b):
+            offset += stride
+            # pprint(reg_a)
+            # pprint(reg_b)
+            if not cls.compare_regfield(reg_a, reg_b, offset):
+                return False
+        return True
+
     def _transform_groups(self, device: OMDeviceMap,
                           groups: Dict[str, Tuple[Dict[str, OMRegField], int]],
                           bitsize: int) -> \
             Tuple[OMDeviceMap, Dict[str, Tuple[Dict[str, OMRegField], int]]]:
         newgroups = {}
-        ffirst = None
-        fcount = 0
-        fdesc = ''
+        gfirst = None
+        gcount = 0
+        gdescs = []
+        blocknames = 'priority enables'.split()
+        in_block = ''
+        D = device.name == 'plic'
+        def debug(*args):
+            if D:
+                print(*args)
+        def pdebug(*args):
+            if D:
+                pprint(*args, sort_dicts=False)
         for name, (group, repeat) in groups.items():
-            # print(name)
-            if name.startswith('priority'):
+            if in_block:
+                if not name.startswith(in_block):
+                    if gcount:
+                        ngroup = [OMRegField(f.offset, f.size, d.strip(),
+                                             f.reset, f.access)
+                                  for f, d in zip(gfirst, gdesc)]
+                        newgroups[bname] = ({bname: ngroup}, gcount)
+                        debug(f'block commit w/ {bname}')
+                        pdebug(newgroups[bname])
+                        gfirst = None
+                        gcount = 0
+                        gdesc = []
+                    in_block = None
+            if not in_block:
+                for bname in blocknames:
+                    if name.startswith(bname):
+                        debug(f'block enter w/ {bname}')
+                        in_block = bname
+                        break
+                else:
+                    debug(f'find no new block from {name}')
+            if in_block:
                 if len(group) != 1:
-                    raise ValueError('Unexpected priority field count')
-                fname = list(group)[0]
-                field = group[fname]
-                if not ffirst:
-                    ffirst = field
-                    fcount = 1
-                    fdesc = field.desc
+                    pdebug(group)
+                #    raise ValueError(f'Unexpected {bname} field count: '
+                #                     f'{len(group)}')
+                fname = commonprefix(tuple(group)).rstrip('_')
+                debug(f'fname {fname}')
+                if not gfirst:
+                    gfirst = list(group.values())
+                    gcount = 1
+                    gdesc = [f.desc for f in gfirst]
                     continue
                 # sanity check: all grouped register should be identical
-                if (field.size != ffirst.size or
-                    field.reset != ffirst.reset or
-                    field.access != ffirst.access or
-                    field.offset != ffirst.offset + fcount * 32):
-                    raise ValueError('Unexpected priority deviation')
-                fcount += 1
-                fdesc = commonprefix((fdesc, field.desc))
+                gregs = list(group.values())
+                if not self.compare_regfield_group(gfirst, gregs, gcount * 32):
+                    raise ValueError(f'Unexpected {bname} deviation')
+                gdesc = [commonprefix((d, f.desc))
+                         for d, f in zip(gdesc, gregs)]
+                gcount += 1
                 continue
-            if fcount:
-                greg = OMRegField(ffirst.offset, ffirst.size, fdesc.strip(),
-                                  ffirst.reset, ffirst.access)
-                newgroups['priority'] = ({'priority': greg}, fcount)
-                ffirst = None
-                fcount = 0
-                fdesc = ''
             newgroups[name] = (group, repeat)
         return device, newgroups
