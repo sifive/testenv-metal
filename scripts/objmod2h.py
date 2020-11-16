93c6acfe60b5b9da8a843620b659658ca0e9b075
Emmanuel Blot <emmanuel.blot@sifive.com>
2020-11-16 16:26:54 +0100

    Use the logging API

--- scripts/objmod2h.py
+++ scripts/objmod2h.py
@@ -9,6 +9,7 @@
 
 from argparse import ArgumentParser, FileType
 from importlib import import_module
+from logging import DEBUG, ERROR, Formatter, StreamHandler, getLogger
 from os import listdir, makedirs
 from os.path import basename, dirname, isdir, isfile, join as joinpath, splitext
 from sys import exit as sysexit, modules, stdin, stdout, stderr
@@ -16,6 +17,27 @@ from traceback import print_exc
 from typing import Dict, Type
 from omtools.generator import OMHeaderGenerator
 from omtools.parser import OMParser
+
+
+def configure_logger(verbosity: int, debug: bool) -> None:
+    """Configure logger format and verbosity.
+
+       :param verbosity: the verbosity level
+       :param debug: debug mode
+    """
+    loglevel = max(DEBUG, ERROR - (10 * (verbosity or 0)))
+    loglevel = min(ERROR, loglevel)
+    if debug:
+        formatter = Formatter('%(asctime)s.%(msecs)03d %(levelname)-7s '
+                              '%(name)-16s %(funcName)s[%(lineno)4d] '
+                              '%(message)s', '%H:%M:%S')
+    else:
+        formatter = Formatter('%(message)s')
+    log = getLogger('om')
+    log.setLevel(loglevel)
+    handler = StreamHandler(stderr)
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
 
 
 def main(args=None) -> None:
@@ -30,41 +52,49 @@ def main(args=None) -> None:
 
         argparser.add_argument('comp', nargs='*',
                                help='Component(s) to extract from model')
-        argparser.add_argument('-i', '--input', type=FileType('rt'),
-                               default=stdin,
-                               help='Input header file')
-        argparser.add_argument('-o', '--output', type=FileType('wt'),
-                               default=stdout,
-                               help='Output header file')
-        argparser.add_argument('-O', '--dir',
-                               help='Output directory')
         argparser.add_argument('-l', '--list', action='store_true',
                                default=False,
                                help=f'List object model devices')
-        argparser.add_argument('-t', '--test', action='store_true',
-                               default=False,
-                               help=f'Generate C test file to check files')
-        argparser.add_argument('-f', '--format', choices=generators,
-                               default=default_gen,
-                               help=f'Output format (default: {default_gen})')
-        argparser.add_argument('-w', '--width', type=int,
-                               choices=(8, 16, 32, 64), default=None,
-                               help='Force register width (default: auto)')
-        argparser.add_argument('-v', '--verbose', action='count', default=0,
-                               help='Increase verbosity')
-        argparser.add_argument('-d', '--debug', action='store_true',
-                               help='Enable debug mode')
+
+        files = argparser.add_argument_group(title='Files')
+        files.add_argument('-i', '--input', type=FileType('rt'),
+                               default=stdin,
+                               help='Input header file')
+        files.add_argument('-o', '--output', type=FileType('wt'),
+                               default=stdout,
+                               help='Output header file')
+        files.add_argument('-O', '--dir',
+                               help='Output directory')
+
+        gen = argparser.add_argument_group(title='Generation')
+        gen.add_argument('-f', '--format', choices=generators,
+                         default=default_gen,
+                         help=f'Output format (default: {default_gen})')
+        gen.add_argument('-w', '--width', type=int,
+                         choices=(8, 16, 32, 64), default=None,
+                         help='Force register width (default: auto)')
+        gen.add_argument('-t', '--test', action='store_true',
+                         default=False,
+                         help=f'Generate C test file to check files')
+
+        extra = argparser.add_argument_group(title='Extras')
+        extra.add_argument('-v', '--verbose', action='count', default=0,
+                           help='Increase verbosity')
+        extra.add_argument('-d', '--debug', action='store_true',
+                           help='Enable debug mode')
 
         args = argparser.parse_args(args)
         debug = args.debug
+        configure_logger(args.verbose, debug)
+        log = getLogger('om.main')
 
         omp = OMParser(debug=debug)
         compnames = [c.lower() for c in args.comp]
         omp.parse(args.input, compnames)
         if args.list:
-            print('Components:', file=args.output)
+            log('Components')
             for comp in omp.device_iterator:
-                print(f' {comp.name}', file=args.output)
+                log.info('%s', comp.name)
             sysexit(0)
         count = 0
         for name in compnames:
@@ -112,24 +142,19 @@ def main(args=None) -> None:
                     else:
                         gen = defgen
                     with open(filename, 'wt') as ofp:
-                        if args.verbose:
-                            print(f'Generating {name} as {filename}',
-                                  file=args.output)
+                        log.info('Generating %s as %s', name, filename)
                         gen.generate_device(ofp, comp, regwidth)
                     header_files.append(basename(filename))
             if compnames:
                 filename = joinpath(args.dir, f'sifive_defs.h')
                 with open(filename, 'wt') as ofp:
                     if args.verbose:
-                        print(f'Generating definition file as {filename}',
-                              file=args.output)
+                        log.info('Generating definition file as %s', filename)
                     gen.generate_definitions(ofp)
                     header_files.append(basename(filename))
             filename = joinpath(args.dir, f'sifive_platform.h')
             with open(filename, 'wt') as ofp:
-                if args.verbose:
-                    print(f'Generating platform file as {filename}',
-                          file=args.output)
+                log.info('Generating platform file as %s', filename)
                 gen.generate_platform(ofp, omp.memory_map, omp.interrupt_map,
                                       omp.xlen)
                 header_files.append(basename(filename))
@@ -148,11 +173,4 @@ def main(args=None) -> None:
 
 
 if __name__ == '__main__':
-    from sys import argv
-    if len(argv) > 1:
-        main()
-    else:
-        # main('-i /Users/eblot/Downloads/s54_fpu_d-arty.objectModel.json -d plic'.split())
-        main('-i /Users/eblot/Downloads/hcaDUT.objectModel.json -d hca'.split())
-        # main('-i /Users/eblot/Downloads/e24_hca.objectModel.json -d hca'.split())
-        # main('-i /Users/eblot/Downloads/s54_fpu_d-arty.objectModel.json -d UART'.split())
+    main()
